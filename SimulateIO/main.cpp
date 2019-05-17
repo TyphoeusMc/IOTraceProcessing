6c57c80a687f44e11c80998449ba9e4a70045e63
unknown <martinfounder@163.com>
2019-05-18 01:05:28 +0800

    修正了标准差的问题，改为变异系数

--- SimulateIO/main.cpp
+++ SimulateIO/main.cpp
@@ -1,5 +1,5 @@
 //本程序模拟多个监控摄像头的IO情况
-//ver 0.1 输出统计量
+//ver 0.2 改进数据的产生，将上下限使用随机数生成
 /**
 1.记录时间占比
 2.平均记录长度
@@ -11,6 +11,7 @@
 8.最短事件长度
 */
 
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -18,24 +19,28 @@
 #include <ctime>
 #include <cmath>
 
-#define tracesNumber 300   //序列的数量
+#define tracesNumber 150   //序列的数量
 #define traceLength 10000   //
-#define shortEventLowerBound 1
-#define shortEventUpperBound 10
-#define longEventLowerBound 11
-#define longEventUpperBound 180
-#define shortIntervalLowerBound 5
-#define shortIntervalUpperBound 25
-#define longIntervalLowerBound 26
-#define longIntervalUpperBound 600
+#define eMinUpper 10
+#define eMinLower 1
+#define eMaxUpper 600
+#define eMaxLower 60
+#define iMinUpper 20
+#define iMinLower 1
+#define iMaxUpper 700
+#define iMaxLower 35
 #define random(x,y) ((rand()%(y-x))+x)
 
 
 using namespace std;
 
-int longEvent = 0;    //每次发生事件持续的时间是否长 0：短 1：混合 2：长
-int longInterval = 0;    //没两次时间之间的间隔是否长  0：短 1：混合 2：长
-bool variate = false;    //各次事件的发生的时间长度差别是否大（相差时间30%）
+int EventLowerBound;
+int EventUpperBound;
+int IntervalLowerBound;
+int IntervalUpperBound;
+
+bool variate = false;    //各次事件的发生的时间长度差别是否大（相差时间30%以内）
+bool variate_i = false;    //间隔长短的差别是否大（相差时间30%以内）
 int timeCounter = 0;    //总时间段之内的秒数计数
 int lastTime;
 
@@ -65,11 +70,12 @@ int main()
     tracePlot.open("C:\\Users\\MartinPC\\Desktop\\TraceCreated\\plot.txt");
     tracePlot<<"header"<<endl;
     ofstream traceStatsFile;
-    traceStatsFile.open("C:\\Users\\MartinPC\\Desktop\\TraceCreated\\stats.txt");
-    traceStatsFile<<"eventRate,aveEventTime,aveIntervalTime,stdV_eventTime,stdV-intTime,eventNumbers,maxEventLength,minEventLength"<<endl;
+    traceStatsFile.open("C:\\Users\\MartinPC\\Desktop\\TraceCreated\\stats.csv");
+    //traceStatsFile<<"eventRate,aveEventTime,aveIntervalTime,stdV_eventTime,stdV-intTime,eventNumbers,maxEventLength,minEventLength"<<endl;
     int timeLeft;
     int timeThisSection;
     int firstEventLength;
+    int firstIntervalLength;
     int actualTime;
     srand((unsigned)time(NULL));
     traceStats Stats;
@@ -80,51 +86,24 @@ int main()
         timeLeft = traceLength;
         timeThisSection = 0;
         firstEventLength = -1;
+        firstIntervalLength = -1;
         //确定本序列属性
-        longEvent = (int)(random(0,30)/10);
-        longInterval = (int)(random(0,30)/10);
-        if(longEvent==1)
-            variate=0;
-        else
-            variate = (int)(random(0,101))>50?true:false;
-        result<<longEvent<<longInterval<<variate<<endl;
+        EventLowerBound=(int)(random(eMinLower,eMinUpper));
+        EventUpperBound=(int)(random(eMaxLower,eMaxUpper));
+        IntervalLowerBound=(int)(random(iMinLower,iMinUpper));
+        IntervalUpperBound=(int)(random(iMaxLower,iMaxUpper));
+        variate = (int)(random(0,101))>70?true:false;       //大部分序列不会齐整
+        variate_i = (int)(random(0,101))>70?true:false;       //大部分序列不会齐整
         //result<<0<<",";
         while(timeLeft>0)
         {
             //先随机一个事件长度
-            switch(longEvent)
-            {
-            case 0:
-                timeThisSection=random(shortEventLowerBound,shortEventUpperBound);
-                break;
-            case 1:
-                timeThisSection=random(shortEventLowerBound,longEventUpperBound);
-                break;
-            case 2:
-                timeThisSection=random(longEventLowerBound,longEventUpperBound);
-                break;
-            default:
-                break;
-            }
+
+            timeThisSection=random(EventLowerBound,EventUpperBound);
             if(variate&&(firstEventLength>=0))
             {
-                while(timeThisSection<int(firstEventLength-firstEventLength*0.3)||timeThisSection>int(firstEventLength+firstEventLength*0.3+1))
-                {
-                    switch(longEvent)
-                    {
-                    case 0:
-                        timeThisSection=random(shortEventLowerBound,shortEventUpperBound);
-                        break;
-                    case 1:
-                        timeThisSection=random(shortEventLowerBound,longEventUpperBound);
-                        break;
-                    case 2:
-                        timeThisSection=random(longEventLowerBound,longEventUpperBound);
-                        break;
-                    default:
-                        break;
-                    }
-                }
+                while(timeThisSection<int(firstEventLength-firstEventLength*0.3)||timeThisSection>int(firstEventLength+firstEventLength*0.3))
+                    timeThisSection=random(EventLowerBound,EventUpperBound);
             }
             if(firstEventLength<0)
                 firstEventLength=timeThisSection;
@@ -150,20 +129,15 @@ int main()
             if(timeLeft<=0)
                 break;
             //后随机一个空闲时间
-            switch(longInterval)
+            timeThisSection=random(IntervalLowerBound,IntervalUpperBound);
+            if(variate_i&&(firstIntervalLength>=0))
             {
-            case 0:
-                timeThisSection=random(shortIntervalLowerBound,shortIntervalUpperBound);
-                break;
-            case 1:
-                timeThisSection=random(shortIntervalLowerBound,longIntervalUpperBound);
-                break;
-            case 2:
-                timeThisSection=random(longIntervalLowerBound,longIntervalUpperBound);
-                break;
-            default:
-                break;
+                while(timeThisSection<int(firstIntervalLength-firstIntervalLength*0.3)||timeThisSection>int(firstIntervalLength+firstIntervalLength*0.3))
+                    timeThisSection=random(IntervalLowerBound,IntervalUpperBound);
             }
+            if(firstIntervalLength<0)
+                firstIntervalLength=timeThisSection;
+
             actualTime=(timeLeft>=timeThisSection)?(lastTime+timeThisSection):traceLength;
             lastTime=actualTime;
             //此处输出的是下一次事件的开始时间
@@ -183,6 +157,9 @@ int main()
         Stats.stdV_intTime=sqrt(Stats.stdV_intTime/Stats.intervalNumbers);
 
         Stats.eventRate=Stats.eventTime/traceLength;
+        Stats.stdV_eventTime/=Stats.aveEventTime;
+        Stats.stdV_intTime/=Stats.aveIntervalTime;
+        Stats.eventNumbers/=traceLength;
         traceStatsFile<<Stats.eventRate<<","<<Stats.aveEventTime<<","<<Stats.aveIntervalTime<<","<<Stats.stdV_eventTime<<",";
         traceStatsFile<<Stats.stdV_intTime<<","<<Stats.eventNumbers<<","<<Stats.maxEventLength<<","<<Stats.minEventLength<<endl;
 
